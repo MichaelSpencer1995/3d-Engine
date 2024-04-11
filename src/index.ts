60e36c0facf9a67b2eddfff80da00772d025209d
Michael Spencer <zequinox117@gmail.com>
2024-04-11 10:06:57 -0500

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { Screen } from './components/screen'
 import { Camera } from './components/camera'
 import { FrameStack } from './components/frame-stack'
-import { Renderer } from './components/renderer'
 import { Settings, SettingsInterface } from './config/settings'
 import { _Frame } from './structures/frame'
-import { EventHandler } from './components/event-handler'
 
 export default class _Entry {
     private $root: string
